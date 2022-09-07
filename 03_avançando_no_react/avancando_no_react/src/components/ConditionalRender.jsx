7dc75f57c53d16823e5a480e5c4acf7ec5624faa
felipeaz3vedo <felipedeazevedopessoa@gmail.com>
2022-09-07 20:26:06 -0300

    aula: 36

--- 03_avançando_no_react/avancando_no_react/src/components/ConditionalRender.jsx
+++ 03_avançando_no_react/avancando_no_react/src/components/ConditionalRender.jsx
@@ -2,12 +2,25 @@ import { useState } from 'react';
 
 export const ConditionalRender = () => {
   const [x] = useState(true);
+  const [name, setName] = useState('');
 
   return (
     <div>
       <h1>Isso será exibido?</h1>
       {x && <p>Se x for true, sim!</p>}
       {!x && <p>Se x for false, não!</p>}
+
+      <h1>If ternário</h1>
+      {name ? (
+        <div>
+          <p>O nome é João</p>
+        </div>
+      ) : (
+        <div>
+          <p>Nome não encontrado</p>
+        </div>
+      )}
+      <button onClick={() => setName('João')}>Altere o nome para João</button>
     </div>
   );
 };
