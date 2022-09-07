78ccbc70d2397a35a72f0bb93f40d64918d1878f
felipeaz3vedo <felipedeazevedopessoa@gmail.com>
2022-09-07 19:56:21 -0300

    aula: 33

--- 03_avançando_no_react/avancando_no_react/src/components/ListRender.jsx
+++ 03_avançando_no_react/avancando_no_react/src/components/ListRender.jsx
@@ -2,12 +2,26 @@ import { useState } from 'react';
 
 export const ListRender = () => {
   const [list] = useState(['Matheus', 'Pedro', 'Josias']);
+  const [users] = useState([
+    { id: 1, name: 'Felipe', age: 30 },
+    { id: 2, name: 'Pedro', age: 31 },
+    { id: 3, name: 'Heitor', age: 18 }
+  ]);
 
   return (
     <div>
-      {list.map(item => (
-        <li>{item}</li>
-      ))}
+      <ul>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {user.name}, {user.age} anos
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
