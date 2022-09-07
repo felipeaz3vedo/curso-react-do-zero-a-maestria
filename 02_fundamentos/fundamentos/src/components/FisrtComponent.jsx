e04bb9f2b209fe6d2c3a53fc5fe66891db26a8c1
felipeaz3vedo <felipedeazevedopessoa@gmail.com>
2022-09-06 21:20:17 -0300

    aula: 20

--- 02_fundamentos/fundamentos/src/components/FisrtComponent.jsx
+++ 02_fundamentos/fundamentos/src/components/FisrtComponent.jsx
@@ -1,6 +1,15 @@
 export const FirstComponent = () => {
+  //comentario de uma linha na parte lógica da função
+
+  /* 
+  comentário de
+  múltiplas linhas na parte lógica da função
+  */
+
   return (
     <div>
+      {/* comentário no JSX */}
+
       <h1>Meu primeiro componente</h1>
       <p className="teste">Meu texto</p>
     </div>
