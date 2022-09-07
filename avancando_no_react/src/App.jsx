8ce42de540f92f0d6a300641c8cdb54add2de092
felipeaz3vedo <felipedeazevedopessoa@gmail.com>
2022-09-07 00:54:14 -0300

    aula: 27 e 28

---
+++ avancando_no_react/src/App.jsx
@@ -0,0 +1,11 @@
+export const App = () => {
+  return (
+    <div>
+      <h1>Avançando em React</h1>
+      <div>
+        {/* imagem em public */}
+        <img src="/vite.svg" alt="Logomarca do Vite" />
+      </div>
+    </div>
+  );
+};
